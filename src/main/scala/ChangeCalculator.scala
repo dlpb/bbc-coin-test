48ce77aa43c8a51a139212846f0f6049794196e6
Daniel Brown <daniel.brown04@bbc.co.uk>
2019-06-27 11:42:26 +0100

    introducing quantiy of denomination

--- src/main/scala/ChangeCalculator.scala
+++ src/main/scala/ChangeCalculator.scala
@@ -1,15 +1,18 @@
-class ChangeCalculator {
-  def calculateChange(amount: Int): Map[Int, Int] = {
+import scala.collection.immutable.ListMap
 
-    val denominations = List(200,100,50,20,10,5,2,1)
+class ChangeCalculator(availableDenominations: ListMap[Denomination, Int]) {
 
-    def calculate(amount: Int, denominations: List[Int], change: Map[Int, Int]): Map[Int, Int] = {
+  def calculateChange(amount: Int): Map[Denomination, Int] = {
+
+    def calculate(amount: Int, denominations: ListMap[Denomination, Int], change: Map[Denomination, Int]): Map[Denomination, Int] = {
       if(amount <= 0  || denominations.isEmpty) change
       else {
-        val highestAvailableDenomination = denominations.head
-        val numberOfDenomination = amount / highestAvailableDenomination
-        val remainingAmount = amount - (numberOfDenomination * highestAvailableDenomination)
+        val (highestAvailableDenomination, quantityOfDenomination) = getHighestAvailableDenomination(denominations)
 
+        val expectedNumberOfDenomination = amount / highestAvailableDenomination.value
+        val numberOfDenomination = if(quantityOfDenomination >= expectedNumberOfDenomination) expectedNumberOfDenomination else quantityOfDenomination
+
+        val remainingAmount = amount - (numberOfDenomination * highestAvailableDenomination.value)
         val newChange =
           if(numberOfDenomination == 0) change
           else change + (highestAvailableDenomination -> numberOfDenomination)
@@ -17,6 +20,26 @@ class ChangeCalculator {
       }
     }
 
-    calculate(amount, denominations, Map())
+    def getHighestAvailableDenomination(denominations: ListMap[Denomination, Int]): (Denomination, Int) = {
+      denominations.maxBy(entry => entry._1.value)
+    }
+
+    calculate(amount, availableDenominations, Map())
   }
+}
+
+case class Denomination(value: Int)
+
+object Denomination {
+  val infiniteAmount = ListMap(
+    Denomination(200)->Int.MaxValue,
+    Denomination(100)->Int.MaxValue,
+    Denomination(50)->Int.MaxValue,
+    Denomination(20)->Int.MaxValue,
+    Denomination(10)->Int.MaxValue,
+    Denomination(5)->Int.MaxValue,
+    Denomination(2)->Int.MaxValue,
+    Denomination(1)->Int.MaxValue
+
+  )
 }
--- src/test/scala/ChangeCalculatorTest.scala
+++ src/test/scala/ChangeCalculatorTest.scala
@@ -1,32 +1,68 @@
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.immutable.ListMap
+
 class ChangeCalculatorTest extends FlatSpec with Matchers {
   "ChangeCalculator" should "calculate change for 1p" in {
-    val cc = new ChangeCalculator
+    val cc = new ChangeCalculator(Denomination.infiniteAmount)
     val change = cc.calculateChange(1)
-    change should be(Map(1->1))
+    change should be(Map(Denomination(1) -> 1))
   }
 
-  it should "calcualte change for 3p using 2p and 1p" in {
-    val cc = new ChangeCalculator
+  it should "calculate change for 3p using 2p and 1p" in {
+    val cc = new ChangeCalculator(Denomination.infiniteAmount)
     val change = cc.calculateChange(3)
-    change should be(Map(2->1,1->1))
+    change should be(Map(Denomination(2) -> 1, Denomination(1) -> 1))
   }
 
   it should "calculate change for £1.23" in {
-    val cc = new ChangeCalculator
+    val cc = new ChangeCalculator(Denomination.infiniteAmount)
     val change = cc.calculateChange(123)
-    change should be(Map(100->1, 20->1, 2->1, 1->1))
+    change should be(
+      Map(
+        Denomination(100) -> 1,
+        Denomination(20) -> 1,
+        Denomination(2) -> 1,
+        Denomination(1) -> 1
+      )
+    )
   }
   it should "calculate change for £9.99" in {
-    val cc = new ChangeCalculator
+    val cc = new ChangeCalculator(Denomination.infiniteAmount)
     val change = cc.calculateChange(999)
-    change should be(Map(
-      200->4,
-      100->1,
-      50->1,
-      20->2,
-      5->1,
-      2->2))
+    change should be(
+      Map(
+        Denomination(200) -> 4,
+        Denomination(100) -> 1,
+        Denomination(50) -> 1,
+        Denomination(20) -> 2,
+        Denomination(5) -> 1,
+        Denomination(2) -> 2
+      )
+    )
+  }
+
+  it should "calculate change for £1.23 when there are no £1 coins available" in {
+    val availableDenominations = ListMap(
+      Denomination(200)->Int.MaxValue,
+      Denomination(100)->0,
+      Denomination(50)->Int.MaxValue,
+      Denomination(20)->Int.MaxValue,
+      Denomination(10)->Int.MaxValue,
+      Denomination(5)->Int.MaxValue,
+      Denomination(2)->Int.MaxValue,
+      Denomination(1)->Int.MaxValue
+    )
+
+    val cc = new ChangeCalculator(availableDenominations)
+    val change = cc.calculateChange(123)
+    change should be(
+      Map(
+        Denomination(50) -> 2,
+        Denomination(20) -> 1,
+        Denomination(2) -> 1,
+        Denomination(1) -> 1
+      )
+    )
   }
 }

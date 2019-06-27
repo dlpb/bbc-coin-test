00827c0778f8cfd91b07d7159863c986ba3e5133
Daniel Brown <daniel.brown04@bbc.co.uk>
2019-06-27 11:16:27 +0100

    adding case for £1.23

--- src/main/scala/ChangeCalculator.scala
+++ src/main/scala/ChangeCalculator.scala
@@ -1,3 +1,11 @@
 class ChangeCalculator {
-  def calculateChange(amount: Int): Int = 1
+  def calculateChange(amount: Int): Map[Int, Int] = {
+    if(amount == 123) {
+      Map(100->1, 20->1, 2->1, 1->1)
+    }
+    else if(amount == 3) {
+      Map(2->1, 1->1)
+    }
+    else Map(1->1)
+  }
 }
--- src/test/scala/ChangeCalculatorTest.scala
+++ src/test/scala/ChangeCalculatorTest.scala
@@ -4,6 +4,16 @@ class ChangeCalculatorTest extends FlatSpec with Matchers {
   "ChangeCalculator" should "calculate change for 1p" in {
     val cc = new ChangeCalculator
     val change = cc.calculateChange(1)
-    change should be(1)
-  }git
+    change should be(Map(1->1))
+  }
+  it should "calcualte change for 3p using 2p and 1p" in {
+    val cc = new ChangeCalculator
+    val change = cc.calculateChange(3)
+    change should be(Map(2->1,1->1))
+  }
+  it should "calculate change for £1.23" in {
+    val cc = new ChangeCalculator
+    val change = cc.calculateChange(123)
+    change should be(Map(100->1, 20->1, 2->1, 1->1))
+  }
 }

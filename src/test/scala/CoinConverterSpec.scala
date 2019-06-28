c06d705825a5c065c3305760fde98e848973677f
Daniel Brown <daniel.brown04@bbc.co.uk>
2019-06-28 16:48:15 +0100

    change contract for coin converter spec

--- src/test/scala/CoinConverterSpec.scala
+++ src/test/scala/CoinConverterSpec.scala
@@ -4,7 +4,7 @@ class CoinConverterSpec extends FlatSpec with Matchers {
 
   "Coin Converter" should "return the correct amount" in {
       val actual = CoinConverter.convertToCoins(123)
-      assert(actual ==  "1 x £1, 1 x 20p, 1 x 2p, 1 x 1p")
+      assert(actual ==  "1 x £1.00, 1 x 20p, 1 x 2p, 1 x 1p")
   }
 
 }
